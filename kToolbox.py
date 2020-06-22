bc632b07b7fbec38c0b877a53f02c127dab3c31d
tubaca <fuller.hill@icloud.com>
2020-06-22 21:39:22 +0800

    Combine mainwindow and kToolBox files

--- kToolbox.py
+++ kToolbox.py
@@ -3,10 +3,74 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import pyqtSlot, Qt
 from krita import *
-from . import mainwindow
 
 highlightedBack = QColor(86, 128, 194)
 back = QColor(49, 49, 49)
+
+class Tool():
+
+    def __init__(self, action, name, shortcut):
+
+        self.action = action
+        self.text = name
+        self.shortcut = shortcut
+
+currentToolList = []
+
+transformTool = Tool("KisToolTransform", "Transform", "(Ctrl + T)")
+moveTool = Tool("KisToolMove", "Move", "(T)")
+cropTool = Tool("KisToolCrop", "Crop", "(C)")
+currentToolList.append(transformTool)
+
+
+shapeSelectTool = Tool("InteractionTool", "Shape Select", "(V)")
+editShapesTool = Tool("VectorTool", "Edit Shapes", "(T)")
+#textTool = Tool("KisToolText", "Text", "(T)")
+calligraphyTool = Tool("KarbonCalligraphyTool", "Calligraphy", "(T)")
+currentToolList.append(shapeSelectTool)
+
+#add actions below
+
+brushTool = Tool("KisToolCrop", "Brush", "(T)")
+dynamicBrushTool = Tool("KisToolCrop", "Dynamic Brush", "(T)")
+multiBrushTool = Tool("KisToolCrop", "Multi Brush", "(T)")
+smartPatchTool = Tool("KisToolCrop", "Smart Patch", "(T)")
+currentToolList.append(brushTool)
+
+fillTool = Tool("KisToolCrop", "Fill", "(T)")
+colorPicker = Tool("KisToolCrop", "Color Picker", "(T)")
+lazyBrushTool = Tool("KisToolCrop", "Lazy Brush", "(T)")
+gradientTool = Tool("KisToolCrop", "Gradient", "(T)")
+currentToolList.append(fillTool)
+
+rectangleTool = Tool("KisToolCrop", "Rectangle", "(T)")
+lineTool = Tool("KisToolCrop", "Line", "(T)")
+freehandTool = Tool("KisToolCrop", "Freehand", "(T)")
+ellipseTool = Tool("KisToolCrop", "Ellipse", "(T)")
+polylineTool = Tool("KisToolCrop", "Polygon", "(T)")
+pathTool = Tool("KisToolCrop", "Path", "(T)")
+currentToolList.append(rectangleTool)
+
+rectangularMarqueeTool = Tool("KisToolCrop", "Rectangular Selection", "(T)")
+circularMarqueeTool = Tool("KisToolCrop", "Elliptical Selection", "(T)")
+polygonalLasso = Tool("KisToolCrop", "Polygonal Selection", "(T)")
+lassoTool = Tool("KisToolCrop", "Freehand Selection", "(T)")
+currentToolList.append(rectangularMarqueeTool)
+
+contiguousSelectionTool = Tool("KisToolCrop", "Contiguous Selection", "(T)")
+similarSelectionTool = Tool("KisToolCrop", "Similar Selection", "(T)")
+magneticLasso = Tool("KisToolCrop", "Magnetic Selection", "(T)")
+bezierCurveLasso = Tool("KisToolCrop", "Path Selection", "(T)")
+currentToolList.append(contiguousSelectionTool)
+
+referenceImagesTool = Tool("KisToolCrop", "Reference Images", "(T)")
+assistantTool = Tool("KisToolCrop", "Assistants", "(T)")
+measureTool = Tool("KisToolCrop", "Measure", "(T)")
+currentToolList.append(referenceImagesTool)
+
+panTool = Tool("KisToolCrop", "Pan", "(T)")
+zoomTool = Tool("KisToolCrop", "Zoom", "(T)")
+currentToolList.append(panTool)
 
 class kToolBox(QDockWidget):
 
@@ -14,10 +78,22 @@ class kToolBox(QDockWidget):
         super(kToolBox, self).__init__()
 
         self.setWindowTitle(i18n("KToolBox"))
+
         mainWidget = QWidget(self)
+
         self.setWidget(mainWidget)
         layout = QVBoxLayout()
         mainWidget.setLayout(layout)
+        for item in currentToolList:
+            button = QPushButton('Tool', self)
+            button.setToolTip('This is an example button')
+            button.move(100,70)
+
+@pyqtSlot()
+def on_click(self):
+    self.connect.paintEvent.drawRectangles(qp)
+
+    button.clicked.connect(self.on_click)
 
     def paintEvent(self, e):
         qp = QPainter()
